49ca987f87fe609320f5b163ca0d014309f5a97e
hugo-gclvs <hugo.gclvs@icloud.com>
2024-01-09 19:22:32 +0100

    Refactor user validation and absence storage logic

--- statlab_api/api/serializers.py
+++ statlab_api/api/serializers.py
@@ -50,74 +50,58 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     @classmethod
     def get_token(cls, user):
-        # Créez un jeton de rafraîchissement avec des informations utilisateur supplémentaires ici si nécessaire
         token = RefreshToken()
-        # Ajoutez des informations personnalisées au jeton ici, si nécessaire
         token['username'] = user['username']
         return token
 
     def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
+        username, password = attrs.get('username'), attrs.get('password')
 
-        # Logique de validation de l'utilisateur
-        if not user_is_valid(username, password):
+        if not self.user_is_valid(username, password):
             raise AuthenticationFailed('No active account found with the given credentials')
 
-        # Récupérer ou créer l'utilisateur dans Firestore
-        user_query = db.collection('users').where('username', '==', username).get()
-        if len(user_query) == 0:
-            user_ref = db.collection('users').document()
-            user_ref.set({"username": username})
-            user = {"username": username}
-        else:
-            user = convert_document_to_dict(user_query[0])
+        user = self.get_or_create_user(username)
+        self.scrape_and_store_user_absences(username, password)
 
-        # Scrapper les informations de l'utilisateur avec OGEScrapper
+        refresh = self.get_token(user)
+        return {'refresh': str(refresh), 'access': str(refresh.access_token)}
+
+    def user_is_valid(self, username, password):
+        return SessionManager(user=username, pwd=password).login()
+
+    def get_or_create_user(self, username):
+        user_query = db.collection('users').where('username', '==', username).get()
+        if not user_query:
+            db.collection('users').document().set({"username": username})
+            return {"username": username}
+        return convert_document_to_dict(user_query[0])
+
+    def scrape_and_store_user_absences(self, username, password):
         session_manager = SessionManager(user=username, pwd=password)
         session_manager.login()
         oge_scraper = OgeScraper(session_manager)
-        absence_service = AbsenceService(oge_scraper)
+        absences = AbsenceService(oge_scraper).getAllAbsences()
 
-        # Récupérer les absences de l'utilisateur sur OGEScrapper
-        absences = absence_service.getAllAbsences()
-        absences = [absence.to_dict() for absence in absences]
-        
-        # remove id from dict
-        for absence in absences:
-            del absence['id']
-        # get the reference to the user
+        self.store_absences_in_firestore([absence.to_dict() for absence in absences], username)
+
+    def store_absences_in_firestore(self, absences, username):
         user_ref = db.collection('users').where('username', '==', username).get()[0].reference
-        # add the reference to the user to each absence
+        subject_type_mapping = {
+            'CM': '/subject_type/CM', 'TD': '/subject_type/TD',
+            'TP': '/subject_type/TP', 'Projet': '/subject_type/Projet'
+        }
+
         for absence in absences:
+            absence.pop('id', None)  # Remove 'id' key if exists
             absence['username'] = user_ref
-        # for each absences get the reference to the subject type
-        for absence in absences:
-            if absence['subjectType'] == 'CM':
-                absence['subjectType'] = '/subject_type/CM'
-            elif absence['subjectType'] == 'TD':
-                absence['subjectType'] = '/subject_type/TD'
-            elif absence['subjectType'] == 'TP':
-                absence['subjectType'] = '/subject_type/TP'
-            elif absence['subjectType'] == 'Projet':
-                absence['subjectType'] = '/subject_type/Projet'
+            absence['subjectType'] = subject_type_mapping.get(absence['subjectType'], absence['subjectType'])
 
-        # Créer les absences dans Firestore
-        for absence in absences:
-            absence_ref = db.collection('absences').document()
-            absence_ref.set(absence)
+            # Check if absence already exists
+            start_date = absence['start_date']
+            end_date = absence['end_date']
+            existing_absences = db.collection('absences').where('username', '==', user_ref) \
+                .where('start_date', '==', start_date).where('end_date', '==', end_date).get()
 
-        # Créer les jetons
-        refresh = self.get_token(user)
-
-        data = {'refresh': str(refresh), 'access': str(refresh.access_token)}
-
-        return data
-    
-def user_is_valid(username, password):
-    session_manager = SessionManager(user=username, pwd=password)
-
-    if session_manager.login():
-        return True
-    else:
-        return False
+            # Add absence if not existing
+            if not existing_absences:
+                db.collection('absences').document().set(absence)
