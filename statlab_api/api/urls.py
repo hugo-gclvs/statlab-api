b6b4dae5c15e2821c7de49524fbe11e395420a35
hugo-gclvs <hugo.gclvs@icloud.com>
2024-01-10 18:41:55 +0100

    Update URLs and add UserAbsencesView

--- statlab_api/api/urls.py
+++ statlab_api/api/urls.py
@@ -1,18 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import AbsenceViewSet, UserViewSet, LoginView
+from .views import UserViewSet, LoginView, UserAbsencesView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 router = DefaultRouter()
-router.register('absences', AbsenceViewSet, basename='absences')
+# router.register('absences', AbsenceViewSet, basename='absences')
 router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
+    path('absences/', UserAbsencesView.as_view(), name='user_absences'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
   
--- statlab_api/api/views.py
+++ statlab_api/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .models import Absence, User
 from .serializers import AbsenceSerializer, UserSerializer, CustomTokenObtainPairSerializer
@@ -42,33 +43,33 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = AbsenceSerializer(absences, many=True)  # Serialize the data
         return Response(serializer.data)
 
-class AbsenceViewSet(viewsets.ModelViewSet):
-    serializer_class = AbsenceSerializer
-    queryset = Absence.objects.all()
+# class AbsenceViewSet(viewsets.ModelViewSet):
+#     serializer_class = AbsenceSerializer
+#     queryset = Absence.objects.all()
 
-    def list(self, request):
-        absences = retrieve_absences()  # Retrieve absences from Firestore
-        serializer = AbsenceSerializer(absences, many=True)  # Serialize the data
-        return Response(serializer.data) 
+#     def list(self, request):
+#         absences = retrieve_absences()  # Retrieve absences from Firestore
+#         serializer = AbsenceSerializer(absences, many=True)  # Serialize the data
+#         return Response(serializer.data) 
 
-    def create(self, request):
-        serializer = AbsenceSerializer(data=request.data)
-        if serializer.is_valid():
-            absence = serializer.save()  # Use the create() method of the serializer
-            return Response(absence)
-        return Response(serializer.errors, status=400)
+#     def create(self, request):
+#         serializer = AbsenceSerializer(data=request.data)
+#         if serializer.is_valid():
+#             absence = serializer.save()  # Use the create() method of the serializer
+#             return Response(absence)
+#         return Response(serializer.errors, status=400)
 
-    def update(self, request, pk=None):
-        existing_absence = retrieve_absence_by_id(pk)
-        serializer = AbsenceSerializer(existing_absence, data=request.data)
-        if serializer.is_valid():
-            updated_absence = serializer.save()  # Use the update() method of the serializer
-            return Response(updated_absence)
-        return Response(serializer.errors, status=400)
+#     def update(self, request, pk=None):
+#         existing_absence = retrieve_absence_by_id(pk)
+#         serializer = AbsenceSerializer(existing_absence, data=request.data)
+#         if serializer.is_valid():
+#             updated_absence = serializer.save()  # Use the update() method of the serializer
+#             return Response(updated_absence)
+#         return Response(serializer.errors, status=400)
 
-    def destroy(self, request, pk=None):
-        # Logic to delete an absence from Firestore
-        return Response({'message': 'Absence successfully deleted!'})
+#     def destroy(self, request, pk=None):
+#         # Logic to delete an absence from Firestore
+#         return Response({'message': 'Absence successfully deleted!'})
 
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
@@ -80,3 +81,15 @@ class LoginView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 
+class UserAbsencesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+#     def get(self, request, username):
+#         username = request.user.username
+#         user_absences = self.get_user_absences(username)
+#         return Response(user_absences)
+
+#     def get_user_absences(self, username):
+#         absences = retrieve_absences_by_user(username)
+#         serializer = AbsenceSerializer(absences, many=True)
+#         return serializer.data
